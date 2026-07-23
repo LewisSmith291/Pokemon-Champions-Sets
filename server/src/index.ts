239f483810493324f5e0772ae6f9cfa3aa0e30e5
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-07-23 11:59:43 +0100

    Filled out middleware pipeline base

---
+++ server/src/index.ts
@@ -0,0 +1,36 @@
+import "dotenv/config"
+import express from "express"
+import cors from "cors"
+import { toNodeHandler } from "better-auth/node"
+import { auth } from "./auth"
+
+const app = express();
+const PORT = Number(process.env.PORT ?? 3001); // Falls back to port 3001 if .env.PORT is null. Convert string into port number
+const CLIENT_URL = process.env.CLIENT_URL ?? "http://localhost:5173"
+
+// Set up CORS
+app.use(
+  cors({
+    origin: CLIENT_URL, // Dev url
+    credentials: true, // Session cookiues can be sent across origins
+  }),
+);
+
+// Better Auth endpoints use different HTTP verbs
+// /api/auth/*splat hands all routes after /auth/... to Better Auth
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+// Only touches routes, rather than messing up Better Auth
+app.use(express.json());
+
+// Health check endpoint. Proves if server is alive
+// '_req' prevents noUnusedParameters from coming up with an error from tsconfig.json as req is unused here
+app.get("/health", (_req, res)=> { 
+  res.json({status: "ok"});
+}); 
+
+// app.listen starts accepting connections on the 'PORT' port number
+app.listen(PORT, () => {
+  console.log(`API listening on https://localhost:${PORT}`);
+  console.log(`Trusting client origin: ${CLIENT_URL}`);
+})
