c818e254e3373d3ab00d049e9c5844fc1f50dfd2
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-07-23 21:25:28 +0100

    Styled header, and made page fit properly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {useSession, signOut} from './services/authClient.ts'
 import AuthForm from './components/organisms/authForm.tsx'
 import CreateSet from './components/organisms/CreateSet.tsx'
+import Header from './components/organisms/Header.tsx';
 
 
 function App() {
@@ -14,12 +15,18 @@ function App() {
 
   // If there is no session
   if (!session){
-    return <AuthForm authMode="signin"/>
+    return (
+      <>
+        <Header />
+        <AuthForm authMode="signin"/>
+      </>
+    )
   }
 
   // Finally, the user is logged in and there is a session
   return (
     <div>
+      <Header />
       <header>
         <span>Signed in as {session.user.name}</span>
         <button onClick={() => signOut()}>Sign Out</button>
---
+++ client/src/components/organisms/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '/Logo.png'
+import './Header.css'
+
+type Props = {}
+
+export default function Header({}: Props) {
+  return (
+    <header>
+      <img id="logo" src={Logo}/>
+    </header>
+  )
+}
