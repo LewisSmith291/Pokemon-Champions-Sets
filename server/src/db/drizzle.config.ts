d9f87031454c0e49a61df837dbc3546fa0010bbf
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-07-23 08:52:43 +0100

    Added drizzle.config.ts

---
+++ server/src/db/drizzle.config.ts
@@ -0,0 +1,9 @@
+import {defineConfig} from "drizzle-kit";
+import "dotenv/config"
+
+export default defineConfig({
+  schema: "./src/db/schema.ts", // Source of table schema
+  out: "./drizzle", // Generated SQL migrations go here (output)
+  dialect: "postgresql",
+  dbCredentials: {url: process.env.DATABASE_URL!} // ! tells TS that it is definately not undefined
+});
