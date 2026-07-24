e57416508c0cd35bc1f45a2a399b7ef1f5cec2dd
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-07-24 20:32:58 +0100

    Added file extension on ./auth.js in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import { toNodeHandler } from "better-auth/node"
-import { auth } from "./auth"
+import { auth } from "./auth.js"
 
 const app = express();
 const PORT = Number(process.env.PORT ?? 3001); // Falls back to port 3001 if .env.PORT is null. Convert string into port number
