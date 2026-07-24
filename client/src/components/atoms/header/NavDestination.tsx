4b2c368438482a4ab391b08a4a544c741671ea0c
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-07-25 00:46:10 +0100

    Fixed centering of nav text

--- client/src/components/atoms/header/NavDestination.tsx
+++ client/src/components/atoms/header/NavDestination.tsx
@@ -5,13 +5,12 @@ interface Props {
 
 export default function NavDestination({name, link}: Props) {
   return (
-    <li>
       <a 
         href={link}
-        className="text-sm font-medium text-white/80 transition-colors hover:text-accent
-        ">
+        className="flex md:text-4xl text-white/80 transition-colors 
+        hover:text-(--color-link-text) hover:bg-pink-500 
+        h-24 w-full p-4 rounded-xl items-center justify-center"  >
         {name}
       </a>
-    </li>
   )
 }
--- client/src/components/molecules/header/HeaderLinks.tsx
+++ client/src/components/molecules/header/HeaderLinks.tsx
@@ -3,14 +3,12 @@ import NavDestination from '../../atoms/header/NavDestination'
 export default function HeaderLinks() {
 
   return (
-    <nav className="flex place-content-center w-full">
-      <ul className="flex flex-row place-content-evenly w-full gap-6">
-        <NavDestination name={"Home"} link="#home"/>
-        <NavDestination name={"Browse"} link="#browse"/>
-        <NavDestination name={"Discover"} link="#discover"/>
-        <NavDestination name={"Search"} link="#search"/>
-        <NavDestination name={"Create"} link="#create"/>
-      </ul>
+    <nav className="flex flex-row place-content-evenly w-full gap-4 px-4 ">
+      <NavDestination name={"Home"} link="#home" />
+      <NavDestination name={"Browse"} link="#browse"/>
+      <NavDestination name={"Discover"} link="#discover"/>
+      <NavDestination name={"Search"} link="#search"/>
+      <NavDestination name={"Create"} link="#create"/>
     </nav>
   )
 }
