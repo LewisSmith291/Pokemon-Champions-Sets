0ff52342c9f3fd30f32e443f83927f29a423c72d
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-07-24 08:52:54 +0100

    Removed unused import, added gengar themed logo, restyled colours

--- client/src/components/organisms/Header.tsx
+++ client/src/components/organisms/Header.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
 import Logo from '/Logo.png'
-
 import './Header.css'
 
-type Props = {}
-
-export default function Header({}: Props) {
+export default function Header() {
   return (
     <header>
       <img id="logo" src={Logo}/>
