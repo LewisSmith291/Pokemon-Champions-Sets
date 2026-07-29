a941fb97c38f98b16f1ae4310a56fd2e3df08be3
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-07-29 22:03:35 +0100

    Edited hero message

--- client/src/components/molecules/Hero.tsx
+++ client/src/components/molecules/Hero.tsx
@@ -1,10 +1,10 @@
 export default function Hero() {
   return (
     <div id="hero" className="flex flex-col">
-      <h2>Welcome to Champion Sets</h2>
+      <h1>Champion Sets</h1>
       <p>Here you can create and share pokemon sets for the game Pokemon Champions. 
         We also have stat-optimisation tools for assistance in set creation!</p>
-        <p>This site is a work in progress</p>
+        <p>This site is a work in progress.</p>
     </div>
   )
 }
