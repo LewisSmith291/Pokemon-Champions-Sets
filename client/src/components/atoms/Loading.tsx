fe612e7b795384663f274e5cf7efde8d7b9fd8b0
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-08-04 20:31:40 +0100

    Restyled page not found and loading icon

--- client/src/components/atoms/Loading.tsx
+++ client/src/components/atoms/Loading.tsx
@@ -3,8 +3,8 @@ import Pokeball from '../../assets/images/Pokeball-icon.svg'
 
 export default function Loading() {
   return (
-    <>
+    <div className="flex flex-1 flex-col gap-6 justify-center items-center">
       <img className="animate-wiggle size-48" src={Pokeball}/>
-    </>
+    </div>
   )
 }
